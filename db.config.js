b783de0b662f02df1d10894356c1217b33c03df4
malayawdg <141549022+malayawdg@users.noreply.github.com>
2023-08-06 11:15:39 +0300

    Add files via upload

---
+++ db.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    HOST: "dpg-cj7latdjeehc73b2jkq0-a",
+    USER: "grady",
+    PORT: 5432,
+    PASSWORD: "EYatZoK3uIq24xrDcJ09Hm0mE5y8ikyt",
+    DB: "malayadb",
+    dialect: "postgres",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
+//
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "postgres",
+//     PORT: 5432,
+//     PASSWORD: "postgres",
+//     DB: "boomboom",
+//     dialect: "postgres",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
